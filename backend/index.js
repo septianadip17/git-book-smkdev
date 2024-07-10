0e869bc54684e4529607426b3bfda2222ed96428
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-10 20:47:43 +0700

    be finish

--- backend/index.js
+++ backend/index.js
@@ -2,24 +2,26 @@ import express from "express";
 import mysql from "mysql";
 import cors from "cors";
 
-
 const app = express();
 
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "book-store",
+  database: "test",
 });
 
-app.get("/", (req, res) => {
-  res.json("Hello ini backend!");
-});
-
-app.use(express.json)
+// biar bisa nambahin data lewat body
+app.use(express.json());
+// biar bisa diambil datanya di frontend, ini dikasih setelah install cors
 app.use(cors());
 
-// mengambil semua data buku
+// testing connect ke backend
+app.get("/", (req, res) => {
+  res.json("hello this is the backend");
+});
+
+// mengambil semua data
 app.get("/books", (req, res) => {
   const q = "SELECT * FROM books";
   db.query(q, (err, data) => {
@@ -28,23 +30,55 @@ app.get("/books", (req, res) => {
   });
 });
 
-// menambahkan data baru
+// post book
 app.post("/books", (req, res) => {
-  const q = "INSERT INTO books (`title`, `desc`, `cover`, `price`) VALUES (?)";
-  const value = [
-    "title from backend",
-    "desc from backend",
-    "cover from backend",
-    "price from backend",
-    10000
+  const q = "INSERT INTO books (`title`, `desc`, `price`, `cover`) VALUES (?)";
+  const values = [
+    // "title from backend",
+    // "desc from backend",
+    // "1234"
+    // "cover from backend"
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
   ];
 
   db.query(q, [values], (err, data) => {
     if (err) return res.json(err);
-    return res.json("berhasil tambah data buku");
-  })
+    return res.json("Book has been created successfully.");
+  });
+});
+
+// delete book
+app.delete("/books/:id", (req, res) => {
+  const bookID = req.params.id;
+  const q = "DELETE FROM books WHERE id =?";
+
+  db.query(q, [bookID], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been deleted successfully.");
+  });
+});
+
+// update book, copy dari delete, values copy dari add
+app.put("/books/:id", (req, res) => {
+  const bookID = req.params.id;
+  const q =
+    "UPDATE books SET `title`=?, `desc`=?, `price`=?, `cover`=? WHERE id=?";
+
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+  db.query(q, [...values, bookID], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been updated successfully.");
+  });
 });
 
 app.listen(8800, () => {
-  console.log("server is running on port 8800");
+  console.log("Server is running on port 8800");
 });
